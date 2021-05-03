ee381f6a0aebef1d22c88ec836107bf663cf9054
Edvard <elsberg1995@protonmail.com>
2021-05-04 01:13:16 +0200

    added stuff

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 using System.IO;
+
+
 
 namespace biblo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Directory<>
+
+
+
         }
     }
     [Serializable]
@@ -20,6 +28,7 @@ namespace biblo
         public int published;
         public int pages;
         public int isbn;
+        private int countOfBook;
 
         public Book(string title, string author, string publisher, string genre, int published, int pages, int isbn){
 
@@ -30,7 +39,97 @@ namespace biblo
             this.published = published;
             this.pages = pages;
             this.isbn = isbn;
+            countOfBook = 1;
+        }
 
+        public void incCount(){
+            countOfBook++;
+        }
+
+        public int decCount(){
+            return --this.countOfBook;
+        }
+
+    }
+    [Serializable]
+    public class User
+    {
+        public string name;
+        public int id;
+        public int booksBorrowed;
+
+        public User(string name, int id, int booksBorrowed){
+
+            this.name = name;
+            this.id = id;
+            this.booksBorrowed = booksBorrowed;
+        }
+    }
+
+    public class Library
+    {
+        private Dictionary<int, Book> books = new Dictionary<int, Book>();
+        private Dictionary<int, User> users = new Dictionary<int, User>();
+        private IFormatter formatter = new BinaryFormatter();
+
+        public void AddBook(int isbn){
+            if(this.books.ContainsKey(isbn)){
+                this.books[isbn].incCount();
+            }else{
+                string title = Console.ReadLine();
+                string author = Console.ReadLine();
+                string publisher = Console.ReadLine();
+                string genre = Console.ReadLine();
+                int published = getInput("Skriv hvad år bogen blev udgivet: ");
+                int pages = getInput("Skriv hvor mange sider bogen har: ");
+                this.books.Add(isbn, new Book(title, author, publisher, genre, published, pages, isbn));
+            }
+        }
+
+        public void RemoveBook(int isbn)
+        {
+            if(this.books.ContainsKey(isbn)){
+                int thisBookCount = this.books[isbn].decCount();
+                if(thisBookCount == 0)
+                    this.books.Remove(isbn);
+            }
+        }
+
+        public void ShowCollection()
+        {
+            if(this.books.Count > 0){
+                foreach(Book book in this.books.Values){
+                    Console.WriteLine(book.title);
+                    Console.WriteLine(book.author);
+                    Console.WriteLine(book.isbn);
+                }
+            }else{
+                Console.WriteLine("Der er ikke nogen bøger i det her bibliotek :(");
+            }
+        }
+
+        public void Export()
+        {
+            Stream stream = new FileStream("library.dat", FileMode.Create, FileAccess.Write);
+            formatter.Serialize(stream, books);
+            stream.Close();
+        }
+
+        public void Import()
+        {
+            Stream stream = new FileStream("library.dat", FileMode.Open, FileAccess.Read);
+            formatter.Serialize(stream, books);
+            stream.Close();
+        }
+
+        static int getInput(string message)
+        {
+            int number;
+            do
+            {
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            return number;
         }
     }
 }
@@ -51,4 +150,3 @@ Kode skal afleveres via github.
 Dokumentation skal ligge i PDF format (PDF'en skal også indeholde klasse diagrammet med beskrivele).
 
 */
-
